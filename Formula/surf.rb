44c2cafdb8c24f1f14b6638fd7d053302ef7811d
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-12 08:11:16 +0900

    updated surf rb

--- Formula/surf.rb
+++ Formula/surf.rb
@@ -1,11 +1,10 @@
 class Surf < Formula
     desc "Surf your local git repository via browser - github.surf"
     homepage "https://github.com/bridgedxyz/surf"
-    url "https://github.com/federico-terzi/espanso/releases/latest/download/espanso-mac.tar.gz"
-    sha256 "2473866b99eef9ea983200b7aac91592b938404ffaa1fb8c72beacb2ebd3203a"
-    version "0.1.0"
+    url "https://github.com/bridgedxyz/surf/archive/0.0.0.tar.gz"
+    sha256 "27ee9abb8be03f749394e78a84957b5ea3d8e403"
+    version "0.0.0"
   
     def install
-      bin.install "espanso"
     end
   end
