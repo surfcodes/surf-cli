a06b7e661689ae21a15b554683c0896cb5f01027
GwonHeeJun <developer_gwon@naver.com>
2021-02-14 00:16:53 +0900

    modify : github initialize check

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # Bridgedxyz Surf
 
 ## How do I install these formulae?
+
 `brew install bridgedxyz/surf`
 
 Or `brew tap bridgedxyz/surf` and then `brew install surf`.
 
 ## Documentation
+
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
+
+## Usage
+
+**Development**
+
+```
+./surf.sh 2> /dev/null
+```
--- surf.sh
+++ surf.sh
@@ -3,6 +3,12 @@
 ROUTE_NAME="$(git config --get remote.origin.url | sed 's/.*\/\([^ ]*\/[^.]*\).*/\1/')"
 MAIN_URL="https://github.surf/"
 
+if [ $(find .git | wc -l) == 0 ]; then
+    echo "There is no github setting."
+    echo "Please proceed with github initial setup."
+    exit 9;
+fi
+
 if [ "$1" == "." ] && [ "$2" == "-b" ]; then
     python -m webbrowser $MAIN_URL$ROUTE_NAME+$3
     exit 2
